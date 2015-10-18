e1f2039b765a3492c18d481d56342dfb8aaa3422
yurareutskiy <yurareutskiy@gmail.com>
2015-10-18 19:40:42 +0300

    Everything is flowing

--- 6HandsSwift/MainTablesViewController.swift
+++ 6HandsSwift/MainTablesViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 
-class MainTablesViewController: UIViewController, CAPSPageMenuDelegate, PopTableViewDelegate, New2TableViewDelegate, UITableViewDelegate {
+class MainTablesViewController: UIViewController, CAPSPageMenuDelegate, PopTableViewDelegate, New2TableViewDelegate, UITableViewDelegate, UITableViewDataSource {
     
     var widthMenu: CGFloat?
     var widthView: CGFloat?
@@ -23,6 +23,7 @@ class MainTablesViewController: UIViewController, CAPSPageMenuDelegate, PopTable
     
     var newVC: PopTableViewController?
     var popularVC: New2TableViewController?
+    var oneMoreVC: PopTableViewController?
     
         
     override func viewDidLoad() {
@@ -45,7 +46,12 @@ class MainTablesViewController: UIViewController, CAPSPageMenuDelegate, PopTable
         rightViewClear!.addGestureRecognizer(rightGestureRecognizer)
         
         navigationController?.navigationBar.addSubview(viewClear!)
-
+        
+        // add clear view
+        
+        view.addSubview(viewClear!)
+        view.addSubview(rightViewClear!)
+        viewClear?.hidden = true
 
         
         // Determine screen size
@@ -58,12 +64,12 @@ class MainTablesViewController: UIViewController, CAPSPageMenuDelegate, PopTable
         // Navigation controller
             // Navigation bar view
         
-        edgesForExtendedLayout = UIRectEdge.None
+//        edgesForExtendedLayout = UIRectEdge.None
         
         
         let pageSliderMenuColor = UIColor(red: 243, green: 243, blue: 244, alpha: 1)
         
-        navigationController?.navigationBar.setBackgroundImage(UIImage(), forBarMetrics: UIBarMetrics.Default)
+//        navigationController?.navigationBar.setBackgroundImage(UIImage(), forBarMetrics: UIBarMetrics.Default)
         navigationController?.view.backgroundColor = pageSliderMenuColor
         navigationController?.navigationBar.shadowImage = UIImage()
         view.backgroundColor = pageSliderMenuColor
@@ -86,30 +92,39 @@ class MainTablesViewController: UIViewController, CAPSPageMenuDelegate, PopTable
         
         // Page slider
         
-        let rectSlider = CGRect(x: 0, y: 0, width: width, height: height - 40)
-        
-        newVC = storyboard?.instantiateViewControllerWithIdentifier("VC with Pop") as? PopTableViewController
-        newVC!.delegate? = self
-        
-        popularVC = storyboard?.instantiateViewControllerWithIdentifier("VC with New") as? New2TableViewController
-        popularVC!.delegate? = self
+        let rectSlider = CGRect(x: 0, y: 0, width: width, height: height)
         
         var arrayVC: [UIViewController] = []
         
-        let controller1 = PopTableViewController()
-        controller1.title = "Новое"
-        arrayVC.append(controller1)
+        newVC = storyboard?.instantiateViewControllerWithIdentifier("VC with Pop") as? PopTableViewController
+        newVC!.delegate? = self
+        newVC?.title = "Популярное"
+        arrayVC.append(newVC!)
         
-        let controller2 = New2TableViewController()
-        controller2.title = "Популярное"
-        arrayVC.append(controller2)
+        popularVC = storyboard?.instantiateViewControllerWithIdentifier("VC with New") as? New2TableViewController
+        popularVC!.delegate? = self
+        popularVC?.title = "Новое"
+        arrayVC.append(popularVC!)
+        
+        
+//        oneMoreVC = storyboard?.instantiateViewControllerWithIdentifier("VC with Pop") as? PopTableViewController
+//        oneMoreVC!.delegate? = self
+//        arrayVC.append(oneMoreVC!)
+        
+//        let controller1 = PopTableViewController()
+//        controller1.title = "Новое"
+//        arrayVC.append(controller1)
+//        
+//        let controller2 = New2TableViewController()
+//        controller2.title = "Популярное"
+//        arrayVC.append(controller2)
         
         let parameters: [CAPSPageMenuOption] = [
             .ScrollMenuBackgroundColor(UIColor.whiteColor()),
             .ViewBackgroundColor(UIColor(red: 247.0/255.0, green: 247.0/255.0, blue: 247.0/255.0, alpha: 1.0)),
             .BottomMenuHairlineColor(UIColor(red: 20.0/255.0, green: 20.0/255.0, blue: 20.0/255.0, alpha: 0.1)),
             .SelectionIndicatorColor(UIColor(red: 18.0/255.0, green: 150.0/255.0, blue: 225.0/255.0, alpha: 1.0)),
-            .MenuMargin(20.0),
+            .MenuMargin(0.0),
             .MenuHeight(40.0),
             .SelectedMenuItemLabelColor(UIColor(red: 18.0/255.0, green: 150.0/255.0, blue: 225.0/255.0, alpha: 1.0)),
             .UnselectedMenuItemLabelColor(UIColor(red: 40.0/255.0, green: 40.0/255.0, blue: 40.0/255.0, alpha: 1.0)),
@@ -117,23 +132,67 @@ class MainTablesViewController: UIViewController, CAPSPageMenuDelegate, PopTable
             .UseMenuLikeSegmentedControl(true),
             .SelectionIndicatorHeight(2.0),
             .MenuItemSeparatorWidth(0),
+            .HideTopMenuBar(false),
         ]
-                
+    
+        
         slider = CAPSPageMenu(viewControllers: arrayVC, frame: rectSlider, pageMenuOptions: parameters)
         slider!.delegate = self
         
+        
         self.view.addSubview(slider!.view)
-        
-        // add clear view
 
-        view.addSubview(viewClear!)
-        view.addSubview(rightViewClear!)
-        viewClear?.hidden = true
         
-        self.navigationController?.addChildViewController(popularVC!)
-        self.navigationController?.addChildViewController(newVC!)
-        let ncArray = navigationController?.viewControllers
-        print(ncArray?.description)
+//        // Array to keep track of controllers in page menu
+//        var controllerArray : [UIViewController] = []
+//
+//        
+//        popularVC = storyboard?.instantiateViewControllerWithIdentifier("VC with New") as? New2TableViewController
+//        popularVC?.tableView.delegate = self
+//        popularVC?.tableView.dataSource = self
+//        controllerArray.append(popularVC!)
+//        
+//        newVC = storyboard?.instantiateViewControllerWithIdentifier("VC with Pop") as? PopTableViewController
+//        newVC?.tableView.delegate = self
+//        newVC?.tableView.dataSource = self
+//        controllerArray.append(newVC!)
+//        
+//        
+//        // Customize page menu to your liking (optional) or use default settings by sending nil for 'options' in the init
+//        // УчфьздуЖ
+//        
+//        let parameters: [CAPSPageMenuOption] = [
+//            .ScrollMenuBackgroundColor(UIColor.whiteColor()),
+//            .ViewBackgroundColor(UIColor(red: 247.0/255.0, green: 247.0/255.0, blue: 247.0/255.0, alpha: 1.0)),
+//            .BottomMenuHairlineColor(UIColor(red: 20.0/255.0, green: 20.0/255.0, blue: 20.0/255.0, alpha: 0.1)),
+//            .SelectionIndicatorColor(UIColor(red: 18.0/255.0, green: 150.0/255.0, blue: 225.0/255.0, alpha: 1.0)),
+//            .MenuMargin(20.0),
+//            .MenuHeight(40.0),
+//            .SelectedMenuItemLabelColor(UIColor(red: 18.0/255.0, green: 150.0/255.0, blue: 225.0/255.0, alpha: 1.0)),
+//            .UnselectedMenuItemLabelColor(UIColor(red: 40.0/255.0, green: 40.0/255.0, blue: 40.0/255.0, alpha: 1.0)),
+//            .MenuItemFont(UIFont(name: "HelveticaNeue-Medium", size: 14.0)!),
+//            .UseMenuLikeSegmentedControl(true),
+//            .SelectionIndicatorHeight(2.0),
+//            .MenuItemSeparatorWidth(0),
+//        ]
+//    
+//        
+//        // Initialize page menu with controller array, frame, and optional parameters
+//        slider = CAPSPageMenu(viewControllers: controllerArray, frame: CGRectMake(0.0, 40.0, self.view.frame.width, self.view.frame.height - 40), pageMenuOptions: parameters)
+//        
+//        // Lastly add page menu as subview of base view controller view
+//        // or use pageMenu controller in you view hierachy as desired
+//        self.view.addSubview(slider!.view)
+
+
+        
+        
+//        self.navigationController?.addChildViewController(popularVC!)
+//        self.navigationController?.addChildViewController(newVC!)
+        
+        self.navigationController?.addChildViewController(slider!)
+        
+
         
          //performSegueWithIdentifier("toRoomPage", sender: self)
         
@@ -269,6 +328,103 @@ class MainTablesViewController: UIViewController, CAPSPageMenuDelegate, PopTable
         self.sideMenuController()?.rightSideMenu?.toggleMenu()
 
     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // MARK: - TABLE VIEW
+    
+    
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete method implementation.
+        // Return the number of rows in the section.
+        return 20
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell : SimpleTableViewCell = tableView.dequeueReusableCellWithIdentifier("TableCell", forIndexPath: indexPath) as! SimpleTableViewCell
+        
+        //        let cell : SimpleTableViewCell = SimpleTableViewCell.init()
+        
+        //        cell.awakeFromNib()
+        
+        //        configure(cell, forRowAtIndexPath: indexPath)
+        
+        cell.someTextLabel!.text = "Карамышевская набережная, 56к2"
+        cell.numerLabel!.text = NSString(format: "%d", indexPath.row + 1) as String
+        cell.priceLabel!.text = formattedStringWithPrice("100000")
+        cell.roomSquareLabel!.text = "123 м²"
+        
+        if indexPath.row % 3 == 0 {
+            cell.yellowCorner.hidden = false
+        }
+        
+        if indexPath.row % 2 == 0 {
+            cell.backgroundCellView.backgroundColor = UIColor.whiteColor()
+        } else {
+            cell.backgroundCellView.backgroundColor = UIColor(patternImage: UIImage(named: "greenBackForCell")!)
+        }
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 60
+    }
+    
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        
+        //        let vc = MainTablesViewController()
+        //        vc.performSegueWithIdentifier("toRoomPage", sender: self)
+        //        let vc = MainTablesViewController()
+        //        vc.toPageMenu()
+        
+        performSegueWithIdentifier("toRoomPage", sender: self)
+        
+    }
+    
+    func configure(cell: SimpleTableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+        
+        cell.someTextLabel!.text = "Карамышевская набережная, 56к2"
+        cell.numerLabel!.text = NSString(format: "%d", indexPath.row + 1) as String
+        cell.priceLabel!.text = formattedStringWithPrice("100000")
+        cell.roomSquareLabel!.text = "123 м²"
+        
+        if indexPath.row % 3 == 0 {
+            cell.yellowCorner.hidden = false
+        }
+        cell.colorStationView.backgroundColor = SubwayLine.Purple.Color
+        if indexPath.row % 2 == 0 {
+            cell.backgroundCellView.backgroundColor = UIColor.whiteColor()
+        } else {
+            cell.backgroundCellView.backgroundColor = UIColor(patternImage: UIImage(named: "greenBackForCell")!)
+        }
+    }
+    
+    
+    func formattedStringWithPrice(price: String) -> String {
+        var lenghtString = price.characters.count
+        var resultString: String = ""
+        for char in price.characters {
+            if (lenghtString % 3 == 0) && (price.characters.count != lenghtString) {
+                resultString += " "
+            }
+            resultString += String(char)
+            lenghtString -= 1
+        }
+        return resultString
+    }
+    
     
     
     /*
